4810990e157cf1e9222ab123b54efaa4345012c7
Olekmi <michonskialeksander@gmail.com>
2019-11-04 21:37:29 +0100

    tanh
    added

--- assignment1_draw.py
+++ assignment1_draw.py
@@ -2,38 +2,38 @@ import assignment1_test as test
 import numpy as np
 import matplotlib.pyplot as plt
 
-iterations = 100000
-stop_criterion = 0.000
-decay_rate = 0.001
+# iterations = 50000
+# stop_criterion = 0.000
+# decay_rate = 0.001
 
-i_r, mean_r = test.neural_network(0.1,decay_rate,iterations,stop_criterion)
-i_b, mean_b = test.neural_network(0.5,decay_rate,iterations,stop_criterion)
-i_g, mean_g = test.neural_network(1,decay_rate,iterations,stop_criterion)
-i_y, mean_y = test.neural_network(3,decay_rate,iterations,stop_criterion)
+# i_r, mean_r = test.neural_network(0.1,decay_rate,iterations,stop_criterion)
+# i_b, mean_b = test.neural_network(0.5,decay_rate,iterations,stop_criterion)
+# i_g, mean_g = test.neural_network(1,decay_rate,iterations,stop_criterion)
+# i_y, mean_y = test.neural_network(3,decay_rate,iterations,stop_criterion)
 
-plt.plot(i_r, mean_r, 'r-', label='alpha = 0.1')
-plt.plot(i_b, mean_b, 'b-', label='alpha = 0.5')
-plt.plot(i_g, mean_g, 'g-', label='alpha = 1')
-plt.plot(i_y, mean_y, 'y-', label='alpha = 3')
-plt.xlabel('iterations')
-plt.ylabel('mean error')
-plt.legend()
-plt.show()
-
-# alpha = []
-# error = []
-# min_error = []
-# for a in range(0,999,5):
-#     alpha_new = 0.01 + a/100 
-#     decay_rate = 0.001
-#     iterations = 6000
-#     stop_criterion = 0.00
-#     i_r, mean_r = test.neural_network(alpha_new,decay_rate,iterations,stop_criterion)
-#     alpha.append(alpha_new)
-#     min_error.append(min(mean_r))
-    
-# plt.plot(alpha, min_error, 'r-', label='decay rate = 0.001')
-# plt.xlabel('alpha')
-# plt.ylabel('minimal error')
+# plt.plot(i_r, mean_r, 'r-', label='alpha = 0.1')
+# plt.plot(i_b, mean_b, 'b-', label='alpha = 0.5')
+# plt.plot(i_g, mean_g, 'g-', label='alpha = 1')
+# plt.plot(i_y, mean_y, 'y-', label='alpha = 3')
+# plt.xlabel('iterations')
+# plt.ylabel('mean error')
 # plt.legend()
 # plt.show()
+
+alpha = []
+error = []
+min_error = []
+for a in range(0,999,5):
+    alpha_new = 0.01 + a/100 
+    decay_rate = 0.001
+    iterations = 6000
+    stop_criterion = 0.00
+    i_r, mean_r = test.neural_network(alpha_new,decay_rate,iterations,stop_criterion)
+    alpha.append(alpha_new)
+    min_error.append(min(mean_r))
+    
+plt.plot(alpha, min_error, 'r-', label='decay rate = 0.001')
+plt.xlabel('alpha')
+plt.ylabel('minimal error')
+plt.legend()
+plt.show()
--- assignment1_test.py
+++ assignment1_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import exp
 
 def normal_distribution(rows,cols):
-    mu, sigma = 0, 0.01 # mean and standard deviation
+    mu, sigma = 0, 0.1 # mean and standard deviation
     s = np.random.normal(mu, sigma, cols)
     for i in range(rows-1):
         s = np.vstack([s,np.random.normal(mu, sigma, cols)])
@@ -10,6 +10,9 @@ def normal_distribution(rows,cols):
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
 
 def neural_network (alpha_0, decay_rate, iterations, stop_criterion):
     input_data = np.eye(8,dtype=int)
@@ -31,12 +34,16 @@ def neural_network (alpha_0, decay_rate, iterations, stop_criterion):
         alpha = (1/(1+decay_rate*i))*alpha_0    
         hidden_node_input = np.dot(input_data ,nodes_weights)
         activation_hidden_layer = sigmoid(hidden_node_input)
+        # activation_hidden_layer = (tanh(hidden_node_input)+1) /2
         activation_hidden_layer  = activation_hidden_layer.T
         bias = np.array([1,1,1,1,1,1,1,1])
         output_hidden_nodes = np.vstack([bias, activation_hidden_layer])
         output_1 = sigmoid(np.dot(output_hidden_nodes.T,output_layer_weights))
+        # output_1 = tanh((np.dot(output_hidden_nodes.T,output_layer_weights)) +1)/2
+
         delta_3 = (output_1 - input_data)
         delta_2 = np.dot(output_layer_weights, delta_3) * output_hidden_nodes * (1 - output_hidden_nodes)
+        # delta_2 = np.dot(output_layer_weights, delta_3)*(1- output_hidden_nodes * output_hidden_nodes)
         output_layer_weights = output_layer_weights - alpha*np.dot( output_hidden_nodes, delta_3)
         nodes_weights = nodes_weights.T - alpha*np.dot(delta_2[1:4,:], input_data.transpose() )
         nodes_weights = nodes_weights.T
